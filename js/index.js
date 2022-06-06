5719cb66394069e41f380cd45a1d992069695977
Robert Cabrera <cabrera.rch@gmail.com>
2022-06-06 10:02:05 +0200

    feat: done

--- js/index.js
+++ js/index.js
@@ -1,15 +1,20 @@
 const firstSelect = document.getElementsByClassName("selects__products")
 const selectProducts = document.createElement("select")
 selectProducts.className = "selects__single"
+selectProducts.id = "select1"
+
 
 const firstSelectProducts = document.createElement("option")
 firstSelectProducts.innerText = "Products (53)"
+firstSelectProducts.value = "products"
 
 const secondSelectProducts = document.createElement("option")
 secondSelectProducts.innerText = "Grifos"
+secondSelectProducts.value = "grifos"
 
 const thirdSelectProducts = document.createElement("option")
 thirdSelectProducts.innerText = "Espejos"
+thirdSelectProducts.value = "espejos"
 
 firstSelect[0].appendChild(selectProducts)
 selectProducts.appendChild(firstSelectProducts)
@@ -17,13 +22,13 @@ selectProducts.appendChild(secondSelectProducts)
 selectProducts.appendChild(thirdSelectProducts)
 
 
-
 const secondSelect = document.getElementsByClassName("selects__sort")
 const orderProducts = document.createElement("select")
 orderProducts.className = "selects__single"
+orderProducts.id = "select2"
 
 const firstOrderProducts = document.createElement("option")
-firstOrderProducts.innerText = "Plateados"
+firstOrderProducts.innerText = "ORDENAR POR"
 
 const secondOrderProducts = document.createElement("option")
 secondOrderProducts.innerText = "Largos"
